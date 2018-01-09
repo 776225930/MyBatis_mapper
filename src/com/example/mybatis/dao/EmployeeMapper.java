f4d4249eb222c39aeee421b337f9c0bd0664309c
Jiang <776225930@qq.com>
2018-01-09 18:00:06 +0800

    多个参数：mybatis会做特殊处理。 	多个参数会被封装成 一个map， 	key：param1...paramN,或者参数的索引也可以 	value：传入的参数值 	#{}就是从map中获取指定的key的值； 	异常： 	org.apache.ibatis.binding.BindingException: 	Parameter 'id' not found. 	Available parameters are [1, 0, param1, param2] 	操作： 	方法：public Employee getEmpByIdAndLastName(Integer id,String lastName); 	取值：#{id},#{lastName} 命名参数：明确指定封装参数时map的key；@Param("id") 	多个参数会被封装成 一个map， 	key：使用@Param注解指定的值 	value：参数值 	#{指定的key}取出对应的参数值

--- src/com/example/mybatis/dao/EmployeeMapper.java
+++ src/com/example/mybatis/dao/EmployeeMapper.java
@@ -1,8 +1,12 @@
 package com.example.mybatis.dao;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.example.mybatis.bean.Employee;
 
 public interface EmployeeMapper {
+	public Employee getEmpByIdAndName(@Param("id") Integer id, @Param("lastName") String lastName);
+
 	public Employee getEmpById(Integer id);
 
 	public void addEmp(Employee employee);
